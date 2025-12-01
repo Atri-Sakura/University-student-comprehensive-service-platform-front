a68fa6317733696e56ec04ec670ae5140e8b7eb7
YuTing789 <485057623@qq.com>
2025-12-01 09:25:06 +0800

    Add files via upload

---
+++ rider/utils/api/feedback.js
@@ -0,0 +1,29 @@
+import request from './request.js'
+
+/**
+ * 获取反馈详情
+ * @param {number} feedbackId 反馈ID
+ */
+export async function getFeedbackInfo(feedbackId) {
+  return request({
+    url: `/rider/feedback/${feedbackId}`,
+    method: 'GET'
+  })
+}
+
+/**
+ * 新增反馈
+ * @param {Object} feedbackData 反馈数据
+ */
+export async function addFeedback(feedbackData) {
+  return request({
+    url: '/rider/feedback',
+    method: 'POST',
+    data: feedbackData
+  })
+}
+
+export default {
+  getFeedbackInfo,
+  addFeedback
+}
