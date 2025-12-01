b6955ac378bbe32da3083e81a8d02a22343a5362
1452hhsbCJH <237825593@qq.com>
2025-12-01 14:36:53 +0800

    加了外卖

--- user/api/index.js
+++ user/api/index.js
@@ -1,9 +1,13 @@
 // API统一导出文件
 import secondhandGoodsApi from './secondhandGoods.js'
 import userApi from './user.js'
+import indexPageApi from './indexPage.js'
+import foodApi from './food.js'
 
 export default {
 	secondhandGoods: secondhandGoodsApi,
-	user: userApi
+	user: userApi,
+	indexPage: indexPageApi,
+	food: foodApi
 }
 
