996024195e2d85d02436aec18510a4474d8b4ccc
YuTing789 <485057623@qq.com>
2025-12-02 15:48:50 +0800

    Update index.js

--- user/api/index.js
+++ user/api/index.js
@@ -3,11 +3,14 @@ import secondhandGoodsApi from './secondhandGoods.js'
 import userApi from './user.js'
 import indexPageApi from './indexPage.js'
 import foodApi from './food.js'
+import orderApi from './order.js'
 
 export default {
 	secondhandGoods: secondhandGoodsApi,
 	user: userApi,
 	indexPage: indexPageApi,
+	order: orderApi,
 	food: foodApi
 }
+
 
